c3c8c6a0bde53e8cc4e0fe4d1a012815f69b8507
Administrator <Administrator@DESKTOP-S9KHPNV>
2024-08-03 21:38:28 +0400

    fix

--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -1,2 +1,23 @@
 ﻿using Terminal;
-Penguin pingvin1 = new Penguin(2.4, "Yellow");
+using static System.Console;
+//Penguin pingvin1 = new Penguin(2.4, "Yellow");
+
+try
+{
+    checked
+    {
+        int x = int.MaxValue - 1;
+        WriteLine($"Initial value: {x}");
+        x++;
+        WriteLine($"After incrementing: {x}");
+        x++;
+        WriteLine($"Affter incrementing: {x}");
+        x++;
+        WriteLine($"Affter incrementing: {x}");
+        x++;
+    }
+}
+catch(OverflowException)
+{
+    WriteLine($"The code overflowed but I caught the exception");
+}
