e12ad9d6ff07042ec18d047ad886024514e44fcd
Administrator <Administrator@DESKTOP-S9KHPNV>
2024-08-17 21:39:35 +0400

    wpfapp added

---
+++ FractionCalulationWpfApp/FractionCalculator/Fraction.cs
@@ -0,0 +1,25 @@
+﻿namespace FractionCalculator;
+public struct Fraction
+{
+    private readonly int _numerator;
+    private readonly int _denominator;
+    public Fraction(int numerator, int denominator)
+    {
+        if (denominator == 0)
+            throw new ArgumentException("Denominator cannot be zero.", nameof(denominator));
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+    public static Fraction operator +(Fraction a) => a;
+    public static Fraction operator -(Fraction a) => new Fraction(-a._numerator, a._denominator);
+    public static Fraction operator +(Fraction a, Fraction b) => new Fraction(a._numerator * b._denominator + a._denominator * b._numerator, a._denominator * b._denominator);
+    public static Fraction operator -(Fraction a, Fraction b) => a + (-b);
+    public static Fraction operator *(Fraction a, Fraction b) => new Fraction(a._numerator * b._numerator, a._denominator * b._denominator);
+    public static Fraction operator /(Fraction a, Fraction b)
+    {
+        if (b._numerator == 0)
+            throw new DivideByZeroException("Denominator cannot be zero.");
+        return new Fraction(a._numerator * b._denominator, a._denominator * b._numerator);
+    }
+    public override string ToString() => $"{_numerator} / {_denominator}";
+}
---
+++ FractionCalulationWpfApp/FractionCalulationWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using FractionCalculator;
+using System.Windows;
+using System.Windows.Controls;
+namespace FractionCalulationWpfApp;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private void btnClear_Click(object sender, RoutedEventArgs e)
+    {
+        num1.Text = "0";
+        den1.Text = "0";
+        num2.Text = "0";
+        den2.Text = "0";
+    }
+
+    private void btnCalculate_Click(object sender, RoutedEventArgs e)
+    {
+        bool isNum1 = int.TryParse(num1.Text, out int intNum1);
+        bool isDen1 = int.TryParse(den1.Text, out int intDen1);
+        bool isNum2 = int.TryParse(num2.Text, out int intNum2);
+        bool isDen2 = int.TryParse(den2.Text, out int intDen2);
+        var selectedOperation = op.SelectedItem as ComboBoxItem;
+        string? operation = selectedOperation?.Content.ToString();
+        if (isNum1 && isDen1 && isNum2 && isDen2)
+        {
+            if (operation != null)
+            {
+                Fraction fraction1 = new Fraction(intNum1, intDen1);
+                Fraction fraction2 = new Fraction(intNum2, intDen2);
+                Fraction fraction3 = new Fraction(1, 1);
+                switch (operation)
+                {
+                    case "+":
+                        Answer.Content = (fraction1 + fraction2).ToString();
+                        break;
+                    case "-":
+                        Answer.Content = (fraction1 - fraction2).ToString();
+                        break;
+                    case "*":
+                        Answer.Content = (fraction1 * fraction2).ToString();
+                        break;
+                    case "/":
+                        Answer.Content = (fraction1 / fraction2).ToString();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter numbers.");
+            }
+            //Fraction fraction1 = new Fraction();
+        }
+    }
+}
