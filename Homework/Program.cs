a4a9fd5f0598f0362d51037eddf68607f0bb6d01
Eduard Snow <eduard.wintersnow@outlook.com>
2024-09-07 20:37:22 +0400

    homework is done

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,7 +1,31 @@
 ﻿using Homework;
 using static System.Console;
-Course english = new Course();
-foreach(var student in english)
+string path = @"C:\Users\MSI PC\Pictures\Screenshots";
+ShowlargrFileWithoutLinq(path);
+static void ShowlargrFileWithoutLinq(string path)
 {
-    WriteLine(student);
+    DirectoryInfo directory = new(path);
+    FileInfo[] files = directory.GetFiles();
+    SortFiles(files);
+    for (int i = 0; i < 5; i++)
+    {
+        WriteLine($"{files[i].Name} : {files[i].Length / 1048576f:F2}MB");
+    }
+}
+static FileInfo[] SortFiles(FileInfo[] folder)
+{
+    FileInfo temp;
+    for (int i = 0; i < folder.Length; i++)
+    {
+        for (int j = i + 1; j < folder.Length; j++)
+        {
+            if (folder[i].Length < folder[j].Length)
+            {
+                temp = folder[i];
+                folder[i] = folder[j];
+                folder[j] = temp;
+            }
+        }
+    }
+    return folder;
 }
--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -7,24 +7,23 @@ worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(worker_WorkPerf
 worker.WorkCompleted += new EventHandler(Worker_WorkCompleted);
 worker.DoWork(23, WorkType.Reports);
 //worker.WorkPerformed += delegate (object sender, WorkPerformedEventArgs e);
-void worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+static void worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
 {
-    WriteLine($"Hours worked: {e.Hours.ToString()} {e.WorkType}");
+    WriteLine($"Hours worked: {e.Hours} {e.WorkType}");
 }
 static void Worker_WorkCompleted(object sender, EventArgs e)
 {
     WriteLine($"Worker is done!");
 }
-
 //2
-string path = @"C:\Users\Administrator\Pictures\Screenshots";
+string path = @"C:\Users\MSI PC\Pictures\Screenshots";
 ShowlargrFileWithoutLinq(path);
 static void ShowlargrFileWithoutLinq(string path)
 {
-    DirectoryInfo directory = new DirectoryInfo(path);
+    DirectoryInfo directory = new(path);
     FileInfo[] files = directory.GetFiles();
-    foreach (FileInfo item in files)
+    foreach (FileInfo file in files)
     {
-        WriteLine($"{item.Name} : {item.Length}");
+        WriteLine($"{file.Name} : {file.Length}");
     }
 }
