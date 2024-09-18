a77a594a984193de929e4ec1bca84c7fbde54609
Eduard Snow <eduard.wintersnow@outlook.com>
2024-09-18 11:41:24 +0400

    fix

--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -3,9 +3,3 @@ using System.Diagnostics.CodeAnalysis;
 using Terminal;
 using static System.Console;
 //1
-//2
-Thread myThread = new(new ThreadStart(WorkingWithThreads.MyThreadMethod));
-//myThread.IsBackground = false;
-myThread.Start();
-//myThread.Join();
-WriteLine("Hello from main thread");
