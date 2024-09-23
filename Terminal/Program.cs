fd9658ccc8e523ab48c4dd90814aa56d5ff5bea3
Administrator <Administrator@DESKTOP-S9KHPNV>
2024-09-23 21:44:21 +0400

    need fixed

--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Collections;
 using Terminal;
 using static System.Console;
 //1
 MyLinkedList<int> d = [5, 8, 3, 4, 7, 9, 2, 1];
- static void PrintMyList(MyLinkedList node)//done
+ static void PrintMyList(MyLinkedListNode<T> node)//done
 {
     while (node != null)
     {
