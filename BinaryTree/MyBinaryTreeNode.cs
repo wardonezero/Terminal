6e6560e7f8b83e56e28384335fd4db71db10a5d0
Administrator <Administrator@DESKTOP-S9KHPNV>
2024-09-30 21:36:29 +0400

    34

---
+++ BinaryTree/MyBinaryTreeNode.cs
@@ -0,0 +1,9 @@
+﻿namespace BinaryTreeProject;
+public class MyBinaryTreeNode<TNode>(TNode value) : IComparable<TNode>
+    where TNode : IComparable<TNode>
+{
+    public TNode Value { get; private set; } = value;
+    public MyBinaryTreeNode<TNode>? Left { get; set; }
+    public MyBinaryTreeNode<TNode>? Right { get; set; }
+    public int CompareTo(TNode? other) => Value.CompareTo(other);
+}
--- VisualQueueWPF/MainWindow.xaml.cs
+++ VisualQueueWPF/MainWindow.xaml.cs
@@ -11,51 +11,45 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
-namespace VisualQueueWPF
+namespace VisualQueueWPF;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    Random random = new Random();
+    MyQueue<int> _list = new MyQueue<int>();
+    public MainWindow()
     {
-        Random random = new Random();
-        MyQueue<int> _list = new MyQueue<int>();
-        public MainWindow()
+        InitializeComponent();
+    }
+    private void DoEnqueue(object sender, RoutedEventArgs e)
+    {
+        _list.Enqueue(random.Next(0, 256));
+        UpdateGrid();
+    }
+    private void DoDequeue(object sender, RoutedEventArgs e)
+    {
+        if (_list.Count > 0)
         {
-            InitializeComponent();
-        }
-
-        private void DoEnqueue(object sender, RoutedEventArgs e)
-        {
-            _list.Enqueue(random.Next(0, 256));
+            listbox.Items.Add(_list.Dequeue().ToString());
             UpdateGrid();
         }
-
-        private void DoDequeue(object sender, RoutedEventArgs e)
+    }
+    private void UpdateGrid()
+    {
+        System.Windows.Controls.Label[] labels = { label0, label1, label2, label3, label4, label5 };
+        MyLinkedListNode<int> current = _list.GetHead();
+        for (int i = 0; i < 6; i++)
         {
-            if (_list.Count > 0)
+            if (current != null && _list.Count > 0)
             {
-                listbox.Items.Add(_list.Dequeue().ToString());
-                UpdateGrid();
+                labels[i].Content = current.Value;
+                current = current.Next;
             }
+            else
+                labels[i].Content = string.Empty;
         }
 
-        private void UpdateGrid()
-        {
-            System.Windows.Controls.Label[] labels = { label0, label1, label2, label3, label4, label5 };
-            MyLinkedListNode<int> current = _list.GetHead();
-            for (int i = 0; i < 6; i++)
-            {
-                if (current != null && _list.Count > 0)
-                {
-                    labels[i].Content = current.Value;
-                    current = current.Next;
-                }
-                else
-                    labels[i].Content = ' ';
-            }
-
-        }
     }
 }
