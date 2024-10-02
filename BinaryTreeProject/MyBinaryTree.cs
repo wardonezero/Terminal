3f207436063bb02647b3de8b244794d65f3c0ba1
Eduard Snow <eduard.wintersnow@outlook.com>
2024-10-02 15:17:17 +0400

    in process

--- BinaryTreeProject/MyBinaryTree.cs
+++ BinaryTreeProject/MyBinaryTree.cs
@@ -4,25 +4,13 @@ namespace BinaryTreeProject;
 public class MyBinaryTree<T> : IEnumerable<T>
     where T : IComparable<T>
 {
-    private readonly MyBinaryTreeNode<T>? _head;
-    private readonly int _count;
+    public MyBinaryTreeNode<T>? _head;
+    private int _count;
     public bool Contains(T item)
     {
         MyBinaryTreeNode<T>? parent;
         return FindWithParent(item, out parent) != null;
     }
-    public IEnumerator<T> GetEnumerator()
-    {
-        throw new NotImplementedException();
-    }
-    /// <summary>
-    /// Finds and returns the first node containing the specified value.  If the value
-    /// is not found, returns null.  Also returns the parent of the found node (or null)
-    /// which is used in Remove.
-    /// </summary>
-    /// <param name="value">The value to search for</param>
-    /// <param name="parent">The parent of the found node (or null)</param>
-    /// <returns>The found node (or null)</returns>
     private MyBinaryTreeNode<T> FindWithParent(T value, out MyBinaryTreeNode<T>? parent)
     {
         parent = null;
@@ -45,8 +33,29 @@ public class MyBinaryTree<T> : IEnumerable<T>
         }
         return current;
     }
-    IEnumerator IEnumerable.GetEnumerator()
+    public bool Remove(T value)
     {
-        throw new NotImplementedException();
+        MyBinaryTreeNode<T> current, parent;
+        current = FindWithParent(value, out parent);
+        if (current == null)
+            return false;
+        _count--;
+        if (current.Right == null)
+        {
+            if (parent.Left == current)
+                parent.Left = current.Left;
+            else
+                parent.Right = current.Left;
+        }
+        else if (current.Right.Left == null)
+        {
+        }
+        else
+            current = null;
+
+        return true;
     }
+    public IEnumerator<T> GetEnumerator() => throw new NotImplementedException();
+    IEnumerator IEnumerable.GetEnumerator() => throw new NotImplementedException();
+    public override string ToString() => $"{_head.Value}";
 }
