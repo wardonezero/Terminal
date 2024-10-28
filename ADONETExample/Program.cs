31168b49b5b3324e118b11b5f9e5a64c17a5877f
Administrator <Administrator@DESKTOP-S9KHPNV>
2024-10-28 21:25:20 +0400

    #43

---
+++ ADONETExample/Program.cs
@@ -0,0 +1,24 @@
+﻿//Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ADONETEXAMPLEDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False
+using static System.Console;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+string ConString = "Data Source=(localdb)\\MSSQLLocalDB;" +
+                   "Initial Catalog=ADONETEXAMPLEDB;" +
+                   "Integrated Security=True;" +
+                   "Connect Timeout=30;" +
+                   "Encrypt=False;" +
+                   "Trust Server Certificate=False;" +
+                   "Application Intent=ReadWrite;" +
+                   "Multi Subnet Failover=False";
+SqlConnection con = new(ConString);
+string querystring = "SELECT * FROM Student";
+con.Open();
+SqlCommand cmd = new(querystring, con);
+SqlDataReader reader = cmd.ExecuteReader();
+while (reader.Read())
+{
+    WriteLine(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString());
+}
+con.Close();
+con.Dispose();
---
+++ ADONETWPF/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Data.SqlClient;
+
+
+namespace ADONETWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string connectionString = @"Data Source=DESKTOP-S9KHPNV\WARDONEZERO;
+                                        Initial Catalog=TSQLV4;
+                                        Integrated Security=True;
+                                        Connect Timeout=30;
+                                        Encrypt=True;
+                                        Trust Server Certificate=True;
+                                        Application Intent=ReadWrite;
+                                        Multi Subnet Failover=False";
+            string sql = "SELECT * FROM HR. Employees";
+            using SqlConnection connection = new(connectionString);
+            connection.Open();
+            SqlDataAdapter adapter = new(sql, connection);
+            DataSet ds = new();
+            adapter.Fill(ds);
+            DataTable dt = ds.Tables[0];
+            dataGrid1.ItemsSource = dt.DefaultView;
+        }
+    }
+}
